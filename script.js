92b515c775ae77711042c99409d89923e21dc64c
bota2202 <otaviosaturnino22@gmail.com>
2025-08-05 12:00:50 -0300

    Alterações

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+let tentativas1, tentavivas2, tentativas3;
+
 function trocaraba(secao){
     window.location.href=secao+".html";
 }
 
+function tentar(fase){
+    if(fase==="1"){
+        let chute=document.getElementById("campo-advinhar").value;
+    }
+}
